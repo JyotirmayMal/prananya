491ef0fddc1d586e7d5be69d6847874d0e02cdd6
JyotirmayMal <ananyamohapatra61@gmail.com>
2025-04-17 15:02:16 +0530

    try 5

--- ml/main.py
+++ ml/main.py
@@ -7,6 +7,10 @@ from jd_api.routes import jobd_bp
 app = Flask(__name__)
 CORS(app, origins=["http://localhost:5173"])
 
+@app.route("/")
+def health_check():
+    return "ML API Running", 200
+
 # Register blueprints
 app.register_blueprint(salary_bp, url_prefix='/salary')
 app.register_blueprint(industry_bp, url_prefix='/industry')
